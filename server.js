8974558471a0b689434f53879cd499ee6a5c829a
Jeyasurya <vjs122334@gmail.com>
2018-01-15 19:24:24 +0530

    added user nanme

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var app = require('express')();
-var http = require('http').server(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get("/",function(req,res){
-    res.send('<h1>Hello World</h1>');
+    res.sendFile(__dirname+'/index.html');
 });
+io.on("connect",function(socket){
+    console.log("user connected");
+    socket.on("post message",function(message){
+    console.log(message.message);
+    console.log(message.username);
+    var messageBody={
+    "message":message.message,
+    "username":message.username
+    }
+    io.emit("broadcast message",messageBody);
+    });
+    socket.on("disconnect",function(socket){
+        console.log("user disconnected");
+    });
+});
+
+
 
 http.listen(3000,function(){
     console.log("listening on port 3000");
